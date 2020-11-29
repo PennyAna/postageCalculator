8e9460f6bda2e15f9f8bec798edcd1ab30846e33
PennyAna <law07006@byui.edu>
2020-11-29 11:18:05 -0700

    adding db

--- server.js
+++ server.js
@@ -5,12 +5,33 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/getPerson', getPerson);
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
-app.get('/db', (req, res) => res.render('pages/db'))
+app.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows: null};
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+})
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Now listening for connections on port: ', app.get('port'));
 });
 
+const {Pool} = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL, 
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 function getPerson(req, res) {
   console.log('getting person info');
   var id = req.query.id;
