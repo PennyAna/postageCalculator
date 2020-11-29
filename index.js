0bf55780fdcf97249e60add07eebe23449dfce32
PennyAna <law07006@byui.edu>
2020-11-28 23:59:49 -0700

    running test run

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express')
-const path = require('path')
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/getPerson', getPerson);
+
+app.listen(app.get('port'), function() {
+  console.log('Now listening for connections on port: ', app.get('port'));
+});
+
+function getPerson(req, res) {
+  console.log('getting person info');
+  var result = (id: 238, first: 'John', last: 'Smith', birthdate: '1983-09-19');
+  res.json(result);
+}
+
+/* const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
@@ -7,4 +22,4 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`)) */
