fcc7d4c4d17d8e16b3ae619b839bacea82bc6165
PennyAna <law07006@byui.edu>
2020-11-29 14:11:41 -0700

    trying again

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ app.use(express.static(__dirname + '/public'))
 app.set('port', (process.env.PORT || 5000))
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
-app.get('/', (req, res) => res.render('pages/index'))
+app.get('/', (req, res) => res.render('pages/index.ejs'))
 app.get('/db', async (req, res) => {
     try {
         const client = await pool.connect();
         const result = await client.query('SELECT * FROM test_table');
         const results = { 'results': (result) ? result.rows: null};
-        res.render('pages/db', results);
+        res.render('pages/db.ejs', results);
         client.release();
     } catch (err) {
         console.error(err);
         res.send("Error " + err);
     }
 })
-app.get('/postage', (req, res) => res.send('public/postage'))
+app.get('/postage', (req, res) => res.render('public/postage.html'))
 
 const {Pool} = require('pg');
 const pool = new Pool({
