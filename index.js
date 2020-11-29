4607f695aba98f5b7ee2c210c4237bc122f4a2da
PennyAna <law07006@byui.edu>
2020-11-29 00:09:08 -0700

    running test run

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const express = require('express')
-var app = express();
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/getPerson', getPerson);
-
-app.listen(app.get('port'), function() {
-  console.log('Now listening for connections on port: ', app.get('port'));
-});
-
-function getPerson(req, res) {
-  console.log('getting person info');
-  var result = {id: 238, first: 'John', last: 'Smith', birthdate: '1983-09-19'};
-  res.json(result);
-}
 
 /* const path = require('path')
 const PORT = process.env.PORT || 5000
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/getPerson', getPerson);
+
+app.listen(app.get('port'), function() {
+  console.log('Now listening for connections on port: ', app.get('port'));
+});
+
+function getPerson(req, res) {
+  console.log('getting person info');
+  var result = {id: 238, first: 'John', last: 'Smith', birthdate: '1983-09-19'};
+  res.json(result);
+}
