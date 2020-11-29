c9240aaef93492e8e881f32b0f912d512818fb8d
PennyAna <law07006@byui.edu>
2020-11-29 00:21:46 -0700

    running test run

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/getPerson', getPerson);
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(app.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
