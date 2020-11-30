fb41e8a86ed7130e296ca9d9dd7db125c568a5a4
PennyAna <law07006@byui.edu>
2020-11-30 12:39:47 -0700

    fixing max, step

--- public/stylesheets/main.js
+++ public/stylesheets/main.js
@@ -1,24 +1,64 @@
 var postal_type = document.getElementById('postalType').value;
 var postal_weight = document.getElementById('postalWeight').value; 
-//input min=0, max=depends on type, step=1
-document.getElementById('postalType').addEventListener('select', setInputMax(postal_type));
-document.getElementById('postalWeight').addEventListener('select', getPostalPrice(postal_weight));
+document.getElementById('typeBtn').addEventListener('click', setInputMax(postal_type));
+document.getElementById('weightBtn').addEventListener('click', getPostalPrice(postal_weight));
 
+//creates postal weight div element (label, btn) w/typeBtn onclick event, calls getWeightInput() to create input element
 function setInputMax(type) {
-    switch(type) {
-        case 1: 
-        case 2: 
-            document.getElementById('postalWeight').max = 3.5;
-            document.getElementById('postalWeight').step = .5;
-            break;
-        case 3:
-        case 4:
-            document.getElementById('postalWeight').max = 13;
-            document.getElementById('postalWeight').step = 1;
-            break;
-    }
+    var weightDiv = document.createElement('div');    
+    weightDiv.classList.add('postalDiv');
+    weightDiv.setAttribute('id', 'weightDiv');
+    weightDiv.setAttribute('name', 'weightDiv');
+    
+    var weightLabel = document.createElement('label');
+    weightLabel.setAttribute('for', 'postalWeight');
+    weightLabel.innerText = 'How heavy is your mail?';
+    weightLabel.classList.add('postalLabel');
+    
+    var weightBtn = document.createElement('button');
+    weightBtn.setAttribute('id', 'weightBtn');
+    weightBtn.setAttribute('name', 'weightBtn');
+    weightBtn.classList.add('postalBtn');
+    weightBtn.setAttribute('value','Get Price')
+
+    var postalWeight = getWeightInput(type);
+
+    weightDiv.appendChild(weightLabel);
+    weightDiv.appendChild(postalWeight); 
+    weightDiv.appendChild(weightBtn);
+    
+    document.getElementById('postalContainer').appendChild(weightDiv);
 }
+//creates postal weight input element (max, step depend on type passed)
+function getWeightInput(type) {
+    var postalWeight = document.createElement('input');
+    postalWeight.setAttribute('type', 'number');
+    postalWeight.setAttribute('id', 'postalWeight');
+    postalWeight.setAttribute('name', 'postalWeight');
+    postalWeight.setAttribute('min', 0);
+    if (type == 1 || type == 2) {
+        postalWeight.setAttribute('max', 3.5);
+        postalWeight.setAttribute('step', .5);
+    }
+    else if (type == 3 || type == 4) {
+        postalWeight.setAttribute('max', 13);
+        postalWeight.setAttribute('step', '1');
+    }
+    return postalWeight;
+}
+//creates postal price p element w/weightBtn onclick event, appends to body
 function getPostalPrice(weight) {
+    var priceDiv = document.createElement('div');    
+    priceDiv.classList.add('postalDiv');
+    priceDiv.setAttribute('id', 'priceDiv');
+    priceDiv.setAttribute('name', 'priceDiv');
+
+    var pPrice = document.createElement('p');
+    pPrice.setAttribute('class', 'finalPrice');
+    pPrice.setAttribute('id', 'finalPrice');
+    pPrice.setAttribute('name', 'finalPrice');
+    var weightNum = Number(postal_weight);
+    
     var type = postal_type;
     var finalPrice = "";
     switch(type) {
@@ -32,10 +72,13 @@ function getPostalPrice(weight) {
         case 4: 
             finalPrice = getPckgPrice(weight);
             break;
-        }
-        document.getElementById('waitingForPrice').innerHTML = "$" + finalPrice;
+    }
+    
+    pPrice.innerText = `Your Price for ${postal_type} with ${weightNum}: $ ${finalPrice};`
+    priceDiv.appendChild(pPrice); 
+    document.body.appendChild(priceDiv);
 }
- 
+
 function getLetterPrice(type, weight) {
     if (weight <= 1) {
         if (type == 1) {
