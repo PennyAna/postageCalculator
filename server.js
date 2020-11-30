0a0193a0c1a935f87b5fe8cfd3032e11125de2ae
PennyAna <law07006@byui.edu>
2020-11-30 00:29:31 -0700

    things displaying, fixing partials links

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const path = require('path')
 var app = express();
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(path.join(__dirname, '/public')))
 app.set('port', (process.env.PORT || 5000))
 app.set('view engine', 'ejs')
 app.get('/', function (req, res) { res.render('public/index.html');})
