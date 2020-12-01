e1f2639872ee3610d6d26829f58fa44faf44403d
PennyAna <law07006@byui.edu>
2020-11-30 21:44:54 -0700

    fixing js, adding css

--- public/stylesheets/main.js
+++ public/stylesheets/main.js
@@ -104,7 +104,7 @@ function getPostalPrice(weight) {
             break;
     }
     
-    pPrice.innerText = "Your Price for " + JSON.stringify(postal_type) + "with " Number(weightNum) ": $" + Number(finalPrice);
+    pPrice.innerText = "Your Price for " + JSON.stringify(postal_type) + "with " + Number(weightNum) + ": $" + Number(finalPrice);
     priceDiv.appendChild(pPrice); 
     document.body.appendChild(priceDiv);
     console.log("Bubbles7")
