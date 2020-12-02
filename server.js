d3e7b153700a88f17decbacb36d0a8a30c30f078
PennyAna <law07006@byui.edu>
2020-12-01 21:43:47 -0700

    still fixing js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 var app = express();
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.static(path.join(__dirname, '/public/stylesheets/main.css')));
+app.use(express.static(path.join(__dirname, '/public/stylesheets')));
 app.set('port', (process.env.PORT || 5000));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
